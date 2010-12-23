532379a6c2f0aa396ada4f0d47bc094fe2102081
Youlian Simidjiyski <ysimidjiyski@uchicago.edu>
2010-12-23 10:50:45 -0500

    Almost works using compound symbols

--- ch2.2/preface.c
+++ ch2.2/preface.c
@@ -11,9 +11,10 @@ TASK: preface
 #include <assert.h>
 
 typedef struct num {
-    int whole[5];  // I, X, C, M, empty  -- Takes values 0-3, OVERFLOW
+    int whole[5];  // I, X, C, M, empty  -- Takes values 0-3
     int half[4];   // V, L, D, empty -- Takes values 0-1 
-    int prefix[5]; // I, X, C, M is being used as a prefix?
+    int c_whole[4]; //IX, XC, CM, empty -- takes values 0-1
+    int c_half[4]; // IV, XL, CD, empty -- takes values 0-1 
 } num_t;
 
 void inc_whole(num_t *num, int loc);
@@ -23,17 +24,14 @@ void inc_half(num_t *num, int loc);
  * increments the given placevalue.
  */
 void inc_whole(num_t *num, int loc){
-    //Are we a prefix?  If so, we go away.
-    if (num->prefix[loc] == 1){
-        num->prefix[loc] = 0;
-        /* Did we have an overflow state like XXXIX?  If so, increment the next
-         * half step, reset the next whole step to 0, and flag a prefix on
-         * the next whole step */ 
-        if (num->whole[loc+1] == OVERFLOW){
-            num->whole[loc+1] = 0;
-            num->prefix[loc+1] = 1;
-            inc_half(num, loc+1);
-        }
+    //Do we have a compound mark that uses us as a prefix?  If so, handle it.
+    if (num->c_whole[loc] == 1){
+        num->c_whole[loc] = 0;
+        inc_whole(num, loc+1);
+    }
+    else if (num->c_half[loc] == 1){
+        num->c_half[loc] = 0;
+        inc_half(num, loc);
     }
     //Not a prefix?  Proceed normally.
     else {
@@ -42,8 +40,13 @@ void inc_whole(num_t *num, int loc){
          * flag a prefix */
         if (num->whole[loc] == 4){
             num->whole[loc] = 0;
-            num->prefix[loc] = 1;
-            inc_half(num, loc);
+            if (num->half[loc] == 0) {
+                num->c_half[loc] = 1;
+            }
+            else {
+                num->half[loc] = 0;
+                num->c_whole[loc] = 1;
+            }
         }
     }
     return;
@@ -51,33 +54,30 @@ void inc_whole(num_t *num, int loc){
 
 void inc_half(num_t *num, int loc){
     num->half[loc]++;
-    int i = num->half[loc];
     //Did we create 2?  If so, increment the next whole setp
-    if (i == 2){
+    if (num->half[loc] == 2){
         num->half[loc] = 0;
-        //are we creating an overflow case?
-        if (num->whole[loc+1] == 3){
-            num->whole[loc+1] = OVERFLOW;
-        }
-        else {
-            inc_whole(num, loc+1);
-        }
+        inc_whole(num, loc+1);
     }
 }
 
+/* Performs an increment on num_inc while tracking the total number of
+ * symbols in num_total
+ */ 
 void inc_all(num_t *num_inc, num_t *num_total){
     int i = 0;
     
     inc_whole(num_inc, 0);
     
     for (i = 0; i < 4; i++){
-        if (num_inc->whole[i] == OVERFLOW){
-            num_total->whole[i] += 4;
+        
+        num_total->whole[i] += num_inc->whole[i];
+        num_total->whole[i] += num_inc->c_whole[i]; //Don't forget compounds!
+        num_total->whole[i] += num_inc->c_half[i]; //or these compounds
+        num_total->half[i] += num_inc->c_half[i]; //or these...
+        if (i > 0){
+            num_total->whole[i] += num_inc->c_whole[i-1]; //more compounds!
         }
-        else{
-            num_total->whole[i] += num_inc->whole[i];
-        }
-        num_total->whole[i] += num_inc->prefix[i]; //Don't forget prefixes!
     }
     for (i = 0; i < 3; i++){
         num_total->half[i] += num_inc->half[i];
@@ -87,26 +87,29 @@ void inc_all(num_t *num_inc, num_t *num_total){
 
 void fprint_num(FILE *stream, num_t num){
 
-    if (num.whole[0] + num.prefix[0] != 0) {
-        fprintf(stream, "I %d\n", num.whole[0] + num.prefix[0]);
+    if (num.whole[0] + num.c_whole[0] + num.c_half[0] != 0) {
+        fprintf(stream, "I %d\n", num.whole[0]+num.c_whole[0]+num.c_half[0]);
     }
-    if (num.half[0] != 0) {
-        fprintf(stream, "V %d\n", num.half[0]);
+    if (num.half[0] + num.c_half[0] != 0) {
+        fprintf(stream, "V %d\n", num.half[0] + num.c_half[0]);
     }
-    if (num.whole[1] + num.prefix[1] != 0) {
-        fprintf(stream, "X %d\n", num.whole[1] + num.prefix[1]);
+    if (num.whole[1]+num.c_whole[0]+num.c_whole[1]+num.c_half[1] != 0) {
+        fprintf(stream, "X %d\n", num.whole[1]+num.c_whole[0]+num.c_whole[1]+
+                num.c_half[1]);
     }
-    if (num.half[1] != 0) {
-        fprintf(stream, "L %d\n", num.half[1]);
+    if (num.half[1]+num.c_half[1] != 0) {
+        fprintf(stream, "L %d\n", num.half[1]+num.c_half[1]);
     }
-    if (num.whole[2] + num.prefix[2] != 0) {
-        fprintf(stream, "C %d\n", num.whole[2] + num.prefix[2]);
+    if (num.whole[2]+num.c_whole[1]+num.c_whole[2]+num.c_half[2] != 0) {
+        fprintf(stream, "C %d\n", num.whole[2]+num.c_whole[1]+num.c_whole[2]+
+                num.c_half[2]);
     }
-    if (num.half[2] != 0) {
-        fprintf(stream, "D %d\n", num.half[2]);
+    if (num.half[2]+num.c_half[2] != 0) {
+        fprintf(stream, "D %d\n", num.half[2]+num.c_half[2]);
     }
-    if (num.whole[3] + num.prefix[3] != 0) {
-        fprintf(stream, "M %d\n", num.whole[3] + num.prefix[3]);
+    if (num.whole[3]+num.c_whole[2]+num.c_whole[3] + num.c_half[3] != 0) {
+        fprintf(stream, "M %d\n", num.whole[3]+num.c_whole[2]+num.c_whole[3]+
+                num.c_half[3]);
     }
 
 }
@@ -124,13 +127,15 @@ int main(){
     num_t numInc, numTotal;
     for (i = 0; i < 5; i++){
         numInc.whole[i] = 0;
-        numInc.prefix[i] = 0;
         numTotal.whole[i] = 0;
-        numTotal.prefix[i] = 0;
     }
     for (i = 0; i < 4; i++){
         numInc.half[i] = 0;
+        numInc.c_whole[i] = 0;
+        numInc.c_half[i] = 0;
         numTotal.half[i] = 0;
+        numTotal.c_whole[i] = 0;
+        numTotal.c_half[i] = 0;
     }
 
 
